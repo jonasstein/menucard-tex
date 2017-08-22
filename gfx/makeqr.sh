d93292ee6e91690b122858cbc16ee5b960a267e2
Jonas Stein <news@jonasstein.de>
2017-08-22 15:26:28 +0200

    Karte wie gedruckt

---
+++ gfx/makeqr.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+qrencode --type SVG -o qr_mecard.svg 'MECARD:N:Yen Yen Asia Spezialitaeten;TEL:+49-2241-1268772;EMAIL: ;URL:http://www.yen-yen.de;ADR:Alte Poststraße 34, 53840 Troisdorf;NICK:Yen Yen;NOTE:An Sonn- und Feiertagen Ruhetag;;' 
